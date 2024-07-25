0b0182d0364f8a31625892996b20150d0f1e8b1c
pvskp <pvincius14@gmail.com>
2024-07-25 13:53:20 -0300

    batman 🦇

---
+++ export.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+base_dir=$(dirname "$(realpath "$0")")
+
+finished_with_errors=0
+
+if [ -L "$HOME/.config/ranger" ]; then
+ echo "Error: ~/.config/ranger/ symbolic link already exists. Delete it first."
+ exit 1
+fi
+
+ln -s "$base_dir/" "$HOME/.config/ranger"
+if [ $? -ne 0 ]; then
+  echo "Error: failed to export ~/.config/ranger/"
+  finished_with_errors=1
+fi
+
+if [ $finished_with_errors -ne 0 ]; then
+  echo "Warning: one or more failed to export"
+else
+  echo "Success: installed ranger config"
+fi
+
+
